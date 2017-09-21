fe34825e9c59a0f003b1c4e817fe8f1985f99c89
Katie B. Williams <katherinebabineau@gmail.com>
2017-09-21 16:32:35 -0700

    Adds the empty view with book and orange

--- app/src/main/java/com/example/katie/bookinventoryapplication/EditorActivity.java
+++ app/src/main/java/com/example/katie/bookinventoryapplication/EditorActivity.java
@@ -1,8 +1,86 @@
 package com.example.katie.bookinventoryapplication;
 
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 /**
  * Created by Katie on 9/14/2017.
  */
 
-public class EditorActivity {
+public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    //Book Data Loader
+    private static final int EXISTING_BOOK_LOADER = 0;
+
+    //Content URI for existing book
+    private Uri mCurrentBookUri;
+
+    //EditText Field to enter the Book Title
+    private EditText mTitleEditText;
+
+    //EditText Field to enter the Book Author
+    private EditText mAuthorEditText;
+
+    //EditText Field to enter the Supplier Email
+    private EditText mSupplierEmailEditText;
+
+    //ImageView for Product Image
+    private ImageView mProductImageView;
+
+    //EditText Field to enter the Price
+    private EditText mPriceEditText;
+
+    //EditText Field to enter the Quantity
+    private EditText mQuantityEditText;
+
+    //EditText Field to enter the Number of Pages
+    private EditText mPageNumberEditText;
+
+    //Boolean to keep track of book edits
+    private boolean mBookHasChanged = false;
+
+
+
+    /**
+     * On Touch Listener for when a user touches a View, implies modification on that view and changes
+     * the boolean above to true. "Book has now changed"
+     */
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener(){
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent){
+            mBookHasChanged = true;
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
--- app/src/main/java/com/example/katie/bookinventoryapplication/MainActivity.java
+++ app/src/main/java/com/example/katie/bookinventoryapplication/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.katie.bookinventoryapplication;
 
 import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+
+import com.example.katie.bookinventoryapplication.data.BookContract;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final int BOOK_LOADER= 0;
     BookCursorAdapter mCursorAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +35,68 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mCursorAdapter = new BookCursorAdapter(this, null);
         bookListView.setAdapter(mCursorAdapter);
 
-        //Setup what happens when a list item is clicked. This should bring you to the details activity
+        //Setup what happens when a list item is clicked. This should bring you to the editor activity
+        //but the editor activity will only show details mode, and won't be editable until another
+        //button is clicked.
 
         getLoaderManager().initLoader(BOOK_LOADER, null, this);
+
     }
 
 
+
+    // Method to test adding dummy data into the database. For debugging purposes
+    private void insertBook(){
+
+        ContentValues values = new ContentValues();
+        values.put(BookContract.BookEntry.COLUMN_BOOK_TITLE, "Beauty");
+        values.put(BookContract.BookEntry.COLUMN_BOOK_PRICE, "1.23");
+        values.put(BookContract.BookEntry.COLUMN_BOOK_QUANTITY, 2);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.action_insert_dummy_data:
+               insertBook();
+               return true;
+            case R.id.action_delete_all_entries:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return null;
+        String[] projection = {
+                BookContract.BookEntry.COLUMN_BOOK_TITLE,
+                BookContract.BookEntry.COLUMN_BOOK_PRICE,
+                BookContract.BookEntry.COLUMN_BOOK_QUANTITY};
+        return new CursorLoader(this,
+                BookContract.BookEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
+
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+      mCursorAdapter.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+      mCursorAdapter.swapCursor(null);
     }
 }
